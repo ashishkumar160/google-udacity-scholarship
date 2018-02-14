a7df82516b877da346d87f24aabf50890c1a9812
Ashish Kumar <ashishkumar160@gmail.com>
2018-02-14 19:54:28 +0530

    Finished First Lesson

---
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/WeatherBox.java
@@ -0,0 +1,21 @@
+package com.example.android.sunshine;
+
+/**
+ * Created by ashish on 14/2/18.
+ */
+
+public class WeatherBox {
+    public static String[] getWeather() {
+        return new String[] {
+                "Freezing",
+                "Windy",
+                "Thunderstorm",
+                "Sunny",
+                "Rainy",
+                "Snow - fall",
+                "Another Weather",
+                "Yet Another Weather",
+                "Seeing if scrolling works"
+        };
+    }
+}
