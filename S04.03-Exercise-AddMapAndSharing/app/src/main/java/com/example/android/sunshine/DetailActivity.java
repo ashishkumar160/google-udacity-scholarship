d68c4960cb9fc9a19ca25a01fb5ecbaddde653c9
Ashish Kumar <ashishkumar160@gmail.com>
2018-02-22 18:22:38 +0530

    Added S04.03

---
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_share) {
+            ShareCompat.IntentBuilder.from(this)
+                    .setChooserTitle(FORECAST_SHARE_HASHTAG)
+                    .setType("text/plain")
+                    .setText(mForecast)
+                    .startChooser();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
