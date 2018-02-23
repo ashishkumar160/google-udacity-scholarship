a0f387e7ed9fb36392aac686dcffd00b1087cd5f
Ashish Kumar <ashishkumar160@gmail.com>
2018-02-24 00:50:53 +0530

    Completed S06.01

---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by ashish on 24/2/18.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // When the home button is pressed, take the user back to the VisualizerActivity
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
